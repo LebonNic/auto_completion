72f74c3795da669349e972b74eed4f3e189e8b88
LebonNic <nicolas.prugne@orange.fr>
2014-02-12 08:44:05 +0100

    Début implémentation de la classe Mot

--- auto_completion/Mot.cpp
+++ auto_completion/Mot.cpp
@@ -1,11 +1,43 @@
 #include "Mot.h"
 
 
-Mot::Mot(void)
+Mot::Mot(const std::string & s)
 {
+	m_Mot = s;
+	m_uOccurences = 0;
 }
-
 
 Mot::~Mot(void)
 {
+}
+
+Mot::Mot(const Mot & m)
+{
+	this->m_Mot = m.m_Mot;
+	this->m_uOccurences = m.m_uOccurences;
+}
+
+Mot & Mot::operator=(const Mot & m)
+{
+	if(this != &m)
+	{
+		this->m_Mot = m.m_Mot;
+		this->m_uOccurences = m.m_uOccurences;
+	}
+	return (*this);
+}
+
+const std::string & Mot::getMot() const
+{
+	return m_Mot;
+}
+
+void Mot::incrementerOccurences()
+{
+	++m_uOccurences;
+}
+
+bool Mot::operator<(const Mot & m)
+{
+	return ((*this) < m);
 }
--- auto_completion/Mot.h
+++ auto_completion/Mot.h
@@ -1,8 +1,22 @@
 #pragma once
+
+#include <string>
+
 class Mot
 {
+private:
+	std::string m_Mot;
+	unsigned int m_uOccurences;
+
 public:
-	Mot(void);
+	Mot(const std::string & s);
+	Mot(const Mot & m);
+	Mot & operator=(const Mot & m);
+	bool operator<(const Mot & m);
+
+	const std::string & getMot() const;
+	void incrementerOccurences();
+
 	~Mot(void);
 };
 
